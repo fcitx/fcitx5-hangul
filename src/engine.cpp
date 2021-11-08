7a77df38a24bd11f1395aa715b7c0bf4bfed14c8
Weng Xuetian <wengxt@gmail.com>
2021-11-08 10:49:29 -0800

    update date

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2010~2017 CSSlayer <wengxt@gmail.com>
+ * SPDX-FileCopyrightText: 2010~2021 CSSlayer <wengxt@gmail.com>
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
