c35d9f9841fbbac34db8c929368867eb6feabdc0
Weng Xuetian <wengxt@gmail.com>
2024-05-27 09:23:30 -0700

    Add test basic case

---
+++ test/testhangul.cpp
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: 2024-2024 CSSlayer <wengxt@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+#include "testdir.h"
+#include "testfrontend_public.h"
+#include <fcitx-utils/eventdispatcher.h>
+#include <fcitx-utils/key.h>
+#include <fcitx-utils/keysym.h>
+#include <fcitx-utils/log.h>
+#include <fcitx-utils/macros.h>
+#include <fcitx-utils/standardpath.h>
+#include <fcitx-utils/testing.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputmethodgroup.h>
+#include <fcitx/inputmethodmanager.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/instance.h>
+
+using namespace fcitx;
+
+void scheduleEvent(Instance *instance) {
+    instance->eventDispatcher().schedule([instance]() {
+        auto *hangul = instance->addonManager().addon("hangul", true);
+        FCITX_ASSERT(hangul);
+        auto defaultGroup = instance->inputMethodManager().currentGroup();
+        defaultGroup.inputMethodList().clear();
+        defaultGroup.inputMethodList().push_back(
+            InputMethodGroupItem("keyboard-us"));
+        defaultGroup.inputMethodList().push_back(
+            InputMethodGroupItem("hangul"));
+        defaultGroup.setDefaultInputMethod("");
+        instance->inputMethodManager().setGroup(defaultGroup);
+        auto *testfrontend = instance->addonManager().addon("testfrontend");
+        auto uuid =
+            testfrontend->call<ITestFrontend::createInputContext>("testapp");
+        auto *ic = instance->inputContextManager().findByUUID(uuid);
+        FCITX_ASSERT(testfrontend->call<ITestFrontend::sendKeyEvent>(
+            uuid, Key("Control+space"), false));
+
+        testfrontend->call<ITestFrontend::pushCommitExpectation>("ㅂ");
+        testfrontend->call<ITestFrontend::pushCommitExpectation>("ㅃ");
+        testfrontend->call<ITestFrontend::pushCommitExpectation>("ㅂ");
+        FCITX_ASSERT(instance->inputMethod(ic) == "hangul");
+
+        FCITX_ASSERT(testfrontend->call<ITestFrontend::sendKeyEvent>(
+            uuid, Key("q"), false));
+        FCITX_ASSERT(testfrontend->call<ITestFrontend::sendKeyEvent>(
+            uuid, Key("Q"), false));
+        FCITX_ASSERT(testfrontend->call<ITestFrontend::sendKeyEvent>(
+            uuid, Key(FcitxKey_Q, KeyState::CapsLock), false));
+        instance->deactivate();
+    });
+
+    instance->eventDispatcher().schedule([instance]() { instance->exit(); });
+}
+
+int main() {
+    setupTestingEnvironment(TESTING_BINARY_DIR, {TESTING_BINARY_DIR "/src"},
+                            {TESTING_BINARY_DIR "/test"});
+    char arg0[] = "testhangul";
+    char arg1[] = "--disable=all";
+    char arg2[] = "--enable=testim,testfrontend,hangul";
+    char *argv[] = {arg0, arg1, arg2};
+    fcitx::Log::setLogRule("default=5,hangul=5");
+    Instance instance(FCITX_ARRAY_SIZE(argv), argv);
+    instance.addonManager().registerDefaultLoader(nullptr);
+    scheduleEvent(&instance);
+    instance.exec();
+
+    return 0;
+}
